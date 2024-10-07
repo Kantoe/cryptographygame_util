89cd3f6901e05b013189b3094a6841f2b90982b2
idokantor <kantor.ido@gmail.com>
2024-10-07 15:59:58 +0300

    final

--- cryptography_game_util.c
+++ cryptography_game_util.c
@@ -13,11 +13,14 @@ int createTCPIpv4Socket()
 
 int createIPv4Address(const char *ip, const int port, struct sockaddr_in *address)
 {
+    int ip_check = 1;
     address->sin_family = AF_INET;
     address->sin_port = htons(port);
-    if(strlen(ip) == NO_IP)
+    if(strlen(ip) == NO_IP) {
         address->sin_addr.s_addr = INADDR_ANY;
-    else
-        return inet_pton(AF_INET,ip , &address->sin_addr.s_addr);
-    return PORT_RANGE_MIN < port && port <= PORT_RANGE_MAX;
+    }
+    else {
+        ip_check = inet_pton(AF_INET,ip , &address->sin_addr.s_addr);
+    }
+    return PORT_RANGE_MIN < port && port <= PORT_RANGE_MAX && ip_check != 0;
 }
