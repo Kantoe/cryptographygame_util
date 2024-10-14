b34f596e9c0684bd79ee75b0bb995b7885502c7b
idokantor <kantor.ido@gmail.com>
2024-10-14 23:40:50 +0300

    final

--- cryptography_game_util.c
+++ cryptography_game_util.c
@@ -26,18 +26,43 @@ int createIPv4Address(const char *ip, const int port, struct sockaddr_in *addres
 }
 
 int execute_command_and_send(const char* command, const int socket_fd) {
-    FILE* fp = popen(command, "r");  // Open the command for reading
-    if(fp == NULL) {
-        perror("popen failed");
+    int pfd[2];  // Pipe file descriptors
+    if (pipe(pfd) < 0) {
+        perror("pipe failed");
         return -1;
     }
-    char output[256];
-    // Read each line from the command output
-    while (fgets(output, sizeof(output), fp) != NULL) {
-        char buffer[260];
+    char full_command[512];
+    snprintf(full_command, sizeof(full_command), "%s 2>&%d", command, pfd[1]);
+    FILE* pout = popen(full_command, "r");
+    if (pout == NULL) {
+        perror("popen failed");
+        close(pfd[1]);
+        return -1;
+    }
+    close(pfd[1]);
+    char output[1024];
+    while (fgets(output, sizeof(output), pout) != NULL) {
+        // Send each line to the socket
+        char buffer[1028]; // +4 for "OUT " prefix
         snprintf(buffer, sizeof(buffer), "OUT %s", output);
         send(socket_fd, buffer, strlen(buffer), 0);
     }
-    pclose(fp);  // Close the file pointer
+
+    FILE* perr = fdopen(pfd[0], "r");
+    if (perr == NULL) {
+        perror("fdopen failed");
+        pclose(pout);
+        return -1;
+    }
+
+    while (fgets(output, sizeof(output), perr) != NULL) {
+        // Send each error line to the socket
+        char buffer[1028]; // +4 for "OUT " prefix
+        snprintf(buffer, sizeof(buffer), "OUT %s", output);
+        send(socket_fd, buffer, strlen(buffer), 0);
+    }
+    // Clean up
+    pclose(pout);
+    fclose(perr);
     return 0;
 }
