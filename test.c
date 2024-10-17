5d05f586bcd6f2328d3b79135b0cee7f13a54c19
idokantor <kantor.ido@gmail.com>
2024-10-17 20:18:29 +0300

    final

--- test.c
+++ test.c
@@ -55,21 +55,20 @@ void printDir()
 void execArgs(char** parsed)
 {
     // Forking a child
-    pid_t pid = fork(); 
+    const pid_t pid = fork();
 
     if (pid == -1) {
         printf("\nFailed forking child..");
         return;
-    } else if (pid == 0) {
+    }
+    if (pid == 0) {
         if (execvp(parsed[0], parsed) < 0) {
             printf("\nCould not execute command..");
         }
         exit(0);
-    } else {
-        // waiting for child to terminate
-        wait(NULL); 
-        return;
     }
+    // waiting for child to terminate
+    wait(NULL);
 }
 
 // Function where the piped system commands is executed
@@ -196,12 +195,9 @@ int parsePipe(char* str, char** strpiped)
         if (strpiped[i] == NULL)
             break;
     }
-
     if (strpiped[1] == NULL)
         return 0; // returns zero if no pipe is found.
-    else {
-        return 1;
-    }
+    return 1;
 }
 
 // function for parsing command words
@@ -255,8 +251,7 @@ int main()
         if (takeInput(inputString))
             continue;
         // process
-        execFlag = processString(inputString,
-        parsedArgs, parsedArgsPiped);
+        execFlag = processString(inputString, parsedArgs, parsedArgsPiped);
         // execflag returns zero if there is no command
         //, or it is a builtin command,
         // 1 if it is a simple command
