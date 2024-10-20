b4b82ddd482f46e509f0fdb4b8642aba6f18c491
idokantor <kantor.ido@gmail.com>
2024-10-21 01:00:30 +0300

    final

--- test.c
+++ test.c
@@ -3,8 +3,6 @@ int main() {
     const char *message = "tlength:38;type:OUT;length:4;data:abcdtlength:39;type:IN;length:6;data:efghij";
     char data [1024] = {0};
     const int check = parse_received_packets(message, data, strlen(message));
-    printf("%ld\n", strlen("tlength:20;type:OUT;length:4;data:abcd"));
-    printf("%ld\n", strlen("tlength:22;type:IN;length:6;data:efghij"));
     if (check != -1) {
         printf("%s\n", data);
     }
